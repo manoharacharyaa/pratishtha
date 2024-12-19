6acf13baf20dd3182aaca24f9ba6c3719231cce5
soham603 <soham.lad16793@sakec.ac.in>
2024-12-19 12:32:57 +0530

    Soham 2.1 InterCollege System - Modals, Backend Complete

---
+++ lib/models/cricketInterCollege.dart
@@ -0,0 +1,62 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class InterCollegeCricketMatch {
+  final String docId;
+  final String teamBattingFirst;
+  final String teamBattingSecond;
+  final String teamBattingFirstScore;
+  final String teamBattingSecondScore;
+  final String teamBattingFirstTopBatter;
+  final String teamBattingFirstTopBowlerPerformance;
+  final String teamBattingSecondTopBatter;
+  final String teamBattingSecondTopBowlerPerformance;
+  final String result;
+  final String matchLocation;
+  final String matchTime;
+  final String matchDayDate;
+  final String matchType;
+  final Bool softDelete;
+
+  InterCollegeCricketMatch({
+    required this.docId,
+    required this.teamBattingFirst,
+    required this.teamBattingSecond,
+    required this.teamBattingFirstScore,
+    required this.teamBattingSecondScore,
+    required this.teamBattingFirstTopBatter,
+    required this.teamBattingFirstTopBowlerPerformance,
+    required this.teamBattingSecondTopBatter,
+    required this.teamBattingSecondTopBowlerPerformance,
+    required this.result,
+    required this.matchLocation,
+    required this.matchTime,
+    required this.matchDayDate,
+    required this.matchType,
+    required this.softDelete,
+  });
+
+  factory InterCollegeCricketMatch.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return InterCollegeCricketMatch(
+      docId: doc.id,
+      teamBattingFirst: data['teamBattingFirst'] ?? '',
+      teamBattingSecond: data['teamBattingSecond'] ?? '',
+      teamBattingFirstScore: data['teamBattingFirstScore'] ?? '',
+      teamBattingSecondScore: data['teamBattingSecondScore'] ?? '',
+      teamBattingFirstTopBatter: data['teamBattingFirstTopBatter'] ?? '',
+      teamBattingFirstTopBowlerPerformance:
+      data['teamBattingFirstTopBowlerPerformance'] ?? '',
+      teamBattingSecondTopBatter: data['teamBattingSecondTopBatter'] ?? '',
+      teamBattingSecondTopBowlerPerformance:
+      data['teamBattingSecondTopBowlerPerformance'] ?? '',
+      result: data['result'] ?? '',
+      matchLocation: data['matchLocation'] ?? '',
+      matchTime: data['matchTime'] ?? '',
+      matchDayDate: data['matchDayDate'] ?? '',
+      matchType: data['matchType'] ?? '',
+      softDelete: data['soft_delete'] ?? false,
+    );
+  }
+}
---
+++ lib/models/interCollege.dart
@@ -0,0 +1,40 @@
+class InterCollege{
+  String id;
+  String collegeName;
+  String collegeShortName;
+  String collegeLocation;
+  int score;
+  String imageUrl;
+  String academicYear;
+  bool softDelete;
+  List<Map<String,int>>? matchesWon;
+
+  InterCollege({
+    required this.id,
+    required this.collegeName,
+    required this.collegeShortName,
+    required this.collegeLocation,
+    required this.score,
+    required this.imageUrl,
+    required this.academicYear,
+    required this.softDelete,
+    this.matchesWon,
+  });
+
+
+  factory InterCollege.fromMap(Map<String, dynamic> data, String id) {
+    return InterCollege(
+      id: id,
+      collegeName: data['collegeName'] ?? '',
+      collegeShortName: data['collegeShortName'] ?? '',
+      collegeLocation: data['collegeLocation'] ?? '',
+      score: data['score'] ?? 0,
+      imageUrl: data['imageUrl'] ?? '',
+      academicYear: data['academicYear'] ?? '',
+      softDelete: data['soft_delete'] ?? false,
+      matchesWon: (data['matchesWon'] as List<dynamic>?)
+          ?.map((item) => Map<String, int>.from(item))
+          .toList(),
+    );
+  }
+}
